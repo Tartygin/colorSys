e2e60cc78a73154f1f02fc2203474d8d3ae56d2a
Tartygin <stas_tartygin@mail.ru>
2021-09-08 22:31:47 +0500

    first commit

---
+++ js/ThemeScript.js
@@ -0,0 +1,27 @@
+const btn = document.querySelector(".btnToggle");
+
+const theme = document.querySelector(".themeLink")
+
+btn.addEventListener('click', function() {
+    if (theme.getAttribute("href") == "css/LightTheme.css"){
+        theme.href = "css/DarkTheme.css"
+        btn.src = "img/Night.svg"
+    }
+    else{
+        theme.href = "css/LightTheme.css"
+        btn.src = "img/Light.svg"
+    }
+});
+
+
+
+const chevron = document.querySelector(".chevron");
+
+chevron.addEventListener('click', function(){
+    if(chevron.getAttribute("src") == "img/chevron-up.svg"){
+        chevron.src = "img/chevron-down.svg"
+    }
+    else{
+        chevron.src = "img/chevron-down.svg"
+    }
+});
